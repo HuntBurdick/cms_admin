ee73a3a8eb7582c7113d7aac6ebb9545c0a0a9c3
Hunt Burdick <huntburdick@Hunts-iMac.local>
2014-12-31 11:01:16 -0700

    more redundant logic.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,13 +12,10 @@ class PagesController < ApplicationController
     @page_title       = label_for_string(@page.name)
     @page_description = @page.body.blank? ? '' : @page.body.truncate(60)
 
-    if @page.only_for_logged_in_members == false || @page.only_for_logged_in_members == true && user_signed_in?
-      @posts = Post.where(:page_id => @page.id, :published => true).order("position ASC")
-      send(@page.name.parameterize) rescue nil
-    else
-      flash[:alert] = "This page is private."
-      redirect_to '/'
-    end
+   
+    @posts = Post.where(:page_id => @page.id, :published => true).order("position ASC")
+    send(@page.name.parameterize) rescue nil
+    
   end
 
   private
