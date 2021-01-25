247cce4fc7df587d7427b3e29c4c2cd8f3894f0b
Hunt Burdick <hunt@edealerdirect.com>
2021-01-25 10:44:18 -0700

    Generator: Create admin view on install.

--- lib/generators/cms_admin/install_generator.rb
+++ lib/generators/cms_admin/install_generator.rb
@@ -5,12 +5,17 @@ module CmsAdmin
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../../templates', __FILE__)
 
+    def add_admin_route
+      route "get 'admin', to: 'admin#index'"
+    end
+
     def create_initializer_file
       create_file "config/initializers/cms_admin_initializer.rb", "# Add initialization content here"
     end
 
-    def add_admin_controller
+    def add_admin_controller_and_view
       template "controllers/admin_controller.erb", "app/controllers/admin_controller.rb"
+      template "views/admin/index.html.erb", "app/views/admin/index.html.erb"
     end
   end
 end
